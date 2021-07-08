6ced4e133a241271fa9eeb2c3dfe7a83765636a2
Your Name <asaadmohamed8756@gmail.com>
2021-07-08 15:13:28 +0200

    Update project

--- api/server.js
+++ api/server.js
@@ -1,22 +1,61 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const countriesData = require("./countriesData.json");
+const PORT = 3000;
 
-var cors = require('cors');
 app.use(cors());
 
-const countriesData = require('./countriesData.json');
+/**
+ * Créations des endpoints/routes
+ */
+app.get("/all", (req, res) => {
+	// On renvoie toutes les données de tous les pays
+	res.json({
+		status: "success",
+		data: countriesData,
+	});
+});
 
-app.get('/all', (req, res) => {
-    res.send({data: countriesData});
+
+// Étape 1
+app.get("/:countryName", (req, res) => {
+	// toLowerCase permet de mettre le string en minuscule pour effectuer la comparaison
+	// avec la liste des pays
+	const countryName = req.params.countryName.toLowerCase();
+
+	// On filtre les données des pays pour récupérer juste l'objet
+	// contenant le pays qui nous intéresse
+	const countryData = countriesData.filter(
+		(country) => country.name.toLowerCase() === countryName
+	);
+
+	res.json({
+		status: "success",
+		data: countryData,
+	});
+});
+
+// Étape 2
+app.get('/:countryCapital', (req, res) => {
+    const countryCapital = req.params.countryCapital.toLocaleLowerCase();
+
+    const countriesData = countriesData.filter(
+        (country) => country.capital.toLowerCase() === countryCapital 
+    );
+
+    res.json({
+        status: "success",
+        data: countryData
+    });
 });
 
 
 
 
-
-
-
-//Run server
-app.listen(300, () => {
-    console.log("Listening on port 300");
+/**
+ * Démarrage du serveur
+ */
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
 });
--- front/js/script.js
+++ front/js/script.js
@@ -1,42 +1,76 @@
-// function getAllCountries() {
+$(() => {
+	// Au démarrage du site, on récupère la liste de tous les pays
+	// et on "branche" une fonction au click du bouton
+	getAllCountries();
+	$("#btnShowData").click(handleClick);
+    $("#radioGroup").click(handleClick);
+});
 
-//     fetch('../api/countriesData.json').then(function (res) {
-//         if(res.ok) {
-//             res.json().then(function (json) {
-//                 countries = json;
-//                 console.log(countries);
-//             });
-//         } else {
-//             console.log("oops, something went wrong");
-//         }
-//     });
-// }
+async function getAllCountries() {
+	const res = await fetch("http://localhost:3000/all");
+	// On transforme la réponse en objet JSON utilisable dans notre code JS
+	const jsonRes = await res.json();
+	console.log(jsonRes);
 
-// getAllCountries();
+	updateList(jsonRes.data);
+}
 
+// Fonction qui permet de mettre à jour la liste en injectant du HTML
+// dans cette dernière
+function updateList(list) {
+	// Effacer la liste existante
+	$("#list").empty();
 
+	// Mise à jour de la liste à l'écran
+	list.forEach((country) =>
+		$("#list").append(
+			`
+            <li class="card">
+                <p>${country.name} - ${country.capital}</p>
+            </li>
+        `
+		)
+	);
+}
 
-// const data = document.getElementById('btnShowData');
-// const { name, capital, currencies, region } = data[Math.floor(Math.random() * 150)];
-const ul = document.getElementById('country')
+// Fonction qui effectue une requête au backend quand on clique
+// sur le bouton Show Data en récupérant la valeur de l'input
+// entrée par l'utilisateur
+async function handleClick() {
+	const userSearchValue = $("#userSearchValue").val();
+	const res = await fetch(`http://localhost:3000/${userSearchValue}`);
+	const jsonRes = await res.json();
+	console.log(jsonRes);
 
-const getAllCountries = async () => {
-    const res = await fetch('../api/countriesData.json')
-    const data = await res.json();
+	updateList(jsonRes.data);
+}
+//////////////////
 
-    return data.map(function (countryData) {
-        let li = createNode('li');
-        data.innerHTML = countryData;
-        append(li);
-        append(ul, li);
-    });
-};
+// Select by capitaly
 
-getAllCountries()
-    .then(data => console.log("resolved:", data))
-    .catch(err => console.log("rejected:", err.message));
+// Fonction qui permet de mettre à jour la liste en injectant du HTML
+// dans cette dernière
+function updateCapital(checkValue) {
+	// Effacer la liste existante
+	$("#checkValue").empty();
 
+	// Mise à jour de la liste à l'écran
+	checkValue.forEach((country) =>
+		$("#checkValue").append(
+			`
+            <li class="card">
+                <p>${document.getElementById('checkValue').checked}</p>
+            </li>
+        `
+		)
+	);
+}
 
+async function handleClick() {
+	const userSearchCapital = $("#userSearchCapital").val();
+	const res = await fetch(`http://localhost:3000/${userSearchCapital}`);
+	const jsonRes = await res.json();
+	console.log(jsonRes);
 
-
-
+	updateCapital(jsonRes.data);
+}
